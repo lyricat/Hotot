905ce72b6d20cd80dee6516a24bcc46b610a37e5
Shellex Y <5h3ll3x@gmail.com>
2010-08-16 16:37:15 +0800

    rename src fold and add launch script and add install scripts.

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+
+from distutils.core import setup
+from DistUtilsExtra.command import *
+from glob import glob
+
+setup(name='hotot',
+      version='0.1.0',
+      description='Lightweight Twitter Client',
+      long_description =
+"""
+Lightweight Twitter Client base on Gtk2 and Webkit.
+
+Features include:
+
+   - Update/View Timelines.
+   - Follow/Unfollow peoples.
+   - Post status.
+   - Reply tweets.
+   - Post direct messages.
+   - View people profile.
+   - Native notification.
+   - Global key-shortcut.
+""",
+      author='Shellex Wai',
+      author_email='5h3ll3x@gmail.com',
+      license='LGPL-3',
+      url="http://code.google.com/p/hotot",
+      download_url="http://code.google.com/p/hotot/downloads/list",
+      platforms = ['Linux'],
+      scripts=['src/hotot.py'],
+      packages = ['src'], 
+      data_files = [
+          ('js', ['js']),
+
+      ],
+      cmdclass = { "build" :  build_extra.build_extra,
+                 }
+      )
+
