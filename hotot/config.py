af2706b6a11d9f55cf8669e6b5c1eb1272c01257
Shellex Wai <5h3ll3x@gmail.com>
2010-08-27 16:46:01 +0800

    ...

--- hotot/config.py
+++ hotot/config.py
@@ -37,7 +37,7 @@ default_config = {
 #api url:
     'api_base': 'http://api.twitter.com/1/',
     'oauth_base': 'https://twitter.com/oauth/',
-    'use_pyrequest'
+    
 #update interval:
     'update_interval': 120,
     'consumer_key': 'SCEdx4ZEOO68QDCTC7FFUQ',
