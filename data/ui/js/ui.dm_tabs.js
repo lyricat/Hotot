49db971e163f02ee4b6168f0355a70df30b59d5e
Shellex Wai <5h3ll3x@gmail.com>
2010-09-09 17:19:53 +0800

    forget to add files

---
+++ data/ui/js/ui.dm_tabs.js
@@ -0,0 +1,26 @@
+if (typeof ui == 'undefined') var ui = {};
+ui.DMTabs = {
+
+current: null,
+
+init:
+function init() {
+    $('#direct_messages_tweet_block .tweet_tabs_btn').click(
+    function (event) {
+        if (! $(this).hasClass('selected')) {
+            // activate another sub page.
+            ui.DMTabs.current = $(this).attr('href');
+            var pagename = ui.DMTabs.current + '_sub_block';
+            $('.tweet_tabs_btn').not(this).removeClass('selected');
+            $(this).addClass('selected');
+            $('.tweet_tabs_page').not(pagename).hide();
+            $(pagename).show();
+            daemon.Updater.update_direct_messages();
+        }
+        return false;
+    });
+    ui.DMTabs.current = '#direct_messages_inbox';
+    $(ui.DMTabs.current + '_sub_block').show();
+},
+
+};
