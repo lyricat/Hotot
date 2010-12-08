c254fb73103a0a8ca01d97cb2f8d248053135477
Jimmy Xu <xu.jimmy.wrk@gmail.com>
2010-12-08 15:06:01 +0000

    issue 210, thanks <adrianperez.deb@gmail.com>

--- hotot/hotot.py
+++ hotot/hotot.py
@@ -43,7 +43,6 @@ class Hotot:
         self.build_inputw()
         if not HAS_INDICATOR:
             self.create_trayicon()
-        pass
 
     def build_gui(self):
         self.window = gtk.Window()
@@ -117,8 +116,7 @@ class Hotot:
         self.window.show()
         self.window.connect('delete-event', gtk.Widget.hide_on_delete)
         self.window.connect('size-allocate', self.on_size_allocate)
-        pass
-    
+
     def build_inputw(self):
         # input window
         self.inputw = gtk.Window()
@@ -140,45 +138,37 @@ class Hotot:
         hbox.show_all() 
         self.inputw.add(hbox)
         self.inputw.connect('delete-event', gtk.Widget.hide_on_delete)
-        pass
 
     def on_btn_update_clicked(self, btn):
         if (self.tbox_status.get_text_length() <= 140):
             agent.update_status(self.tbox_status.get_text())
             self.tbox_status.set_text('')
             self.inputw.hide()
-        pass
 
     def on_tbox_status_changed(self, entry):
         if (self.tbox_status.get_text_length() <= 140):
             entry.modify_base(gtk.STATE_NORMAL, gtk.gdk.Color('#fff'))
         else:
             entry.modify_base(gtk.STATE_NORMAL, gtk.gdk.Color('#f00'))
-        pass
-    
+
     def on_tbox_status_key_released(self, entry, event):
         if event.keyval == gtk.keysyms.Return:
             self.btn_update.clicked();
             entry.stop_emission('insert-text')
-        pass
 
     def on_mitem_resume_activate(self, item):
         self.window.present()
-        pass
 
     def on_mitem_prefs_activate(self, item):
         agent.execute_script('ui.DialogHelper.open(ui.PrefsDlg);');
         self.window.present()
-        pass
 
     def on_mitem_about_activate(self, item):
         agent.execute_script('ui.DialogHelper.open(ui.AboutDlg);');
         self.window.present()
-        pass
 
     def on_mitem_quit_activate(self, item):
         self.quit()
-        pass
 
     def quit(self, *args):
         if self.active_profile != 'default':
@@ -186,11 +176,10 @@ class Hotot:
             config.dump_sys_conf()
         gtk.gdk.threads_leave()
         self.window.destroy()
-        gtk.main_quit() 
+        gtk.main_quit()
         import sys
         sys.exit(0)
-        pass
-        
+
     def init_hotkey(self):
         try:
             keybinder.bind(
@@ -198,7 +187,6 @@ class Hotot:
                 , self.on_hotkey_compose)
         except:
             pass
-        pass
 
     def create_trayicon(self):
         """ 
@@ -211,28 +199,23 @@ class Hotot:
         self.trayicon.set_from_file(
             utils.get_ui_object('imgs/ic64_hotot_classics.png'))
         self.trayicon.set_visible(True)
-        pass
 
     def on_trayicon_activate(self, icon):
         gobject.idle_add(self._on_trayicon_activate, icon)
-        pass
 
     def _on_trayicon_activate(self, icon):
         if self.window.is_active():
             self.window.hide()
         else:
             self.window.present()
-        pass
 
     def on_trayicon_popup_menu(self, icon, button, activate_time):
         self.menu_tray.popup(None, None
             , None, button=button
             , activate_time=activate_time)
-        pass
 
     def on_hotkey_compose(self):
         gobject.idle_add(self._on_hotkey_compose)
-        pass
 
     def _on_hotkey_compose(self):
         if config.get(self.active_profile, 'use_native_input'):
@@ -247,17 +230,15 @@ class Hotot:
                 pass
             self.window.present()
             self.webv.grab_focus()
-        pass
 
     def on_size_allocate(self, win, req):
         if self.is_sign_in:
             config.set(self.active_profile, 'size_h', req.height)
             config.set(self.active_profile, 'size_w', req.width)
-        pass
 
     def on_sign_in(self):
         self.is_sign_in = True
-         
+
         self.window.set_title('Hotot | %s' % self.active_profile)
         self.window.resize(
               config.get(self.active_profile, 'size_w')
@@ -265,11 +246,9 @@ class Hotot:
         self.init_hotkey()
         agent.apply_config()
         agent.init_exts()
-        pass
 
     def on_sign_out(self):
         self.is_sign_in = False
-        pass
 
 def main():
     global HAS_INDICATOR
@@ -295,7 +274,6 @@ def main():
         indicator.set_status(appindicator.STATUS_ACTIVE)
         indicator.set_attention_icon(utils.get_ui_object('imgs/ic64_hotot.png'))
         indicator.set_menu(app.menu_tray)
-        pass
     gtk.gdk.threads_enter()
     gtk.main()
     gtk.gdk.threads_leave()
