0bc58740fa6cecbd50f63cecb73a19ae9fad385e
Shellex Wai <5h3ll3x@gmail.com>
2011-03-11 08:01:17 +0800

    fixed: i18n scanner bug

--- i18n_scanner.py
+++ i18n_scanner.py
@@ -8,7 +8,7 @@ import os.path
 TEMPLATE = "data/index.html"
 LOCALE_FILE_DIR = 'data/_locales/'
 
-template_tag_re = re.compile('data-i18n-[a-z0-9]+="(.+)"')
+template_tag_re = re.compile('data-i18n-[a-z0-9]+="(.+?)"')
 
 def scan_template():
     template_file = open(TEMPLATE, 'r')
@@ -28,12 +28,17 @@ def load_exist_trans(trans_file):
 
 def walk_cb(empty_trans, dir_name, f_names):
     new_trans = ''
+    exists_trans = {}
     if dir_name.endswith('data/_locales/'):
         return
     file_path = os.path.join(dir_name, 'messages.json')
     if 'messages.json' in f_names:
         trans_file = open(file_path, 'r')
-        exists_trans = json.loads(trans_file.read())
+        exists_data = trans_file.read()
+        if exists_data:
+            exists_trans = json.loads(exists_data)
+        else:
+            exists_trans = {}
         trans_file.close()
         new_trans = format_out(merge_trans(empty_trans, exists_trans))
         print '[Update]', file_path
@@ -41,7 +46,7 @@ def walk_cb(empty_trans, dir_name, f_names):
         new_trans = format_out(empty_trans)
         print '[Create]', file_path
     trans_file = open(file_path, 'w+')
-    trans_file.write(new_trans)
+    trans_file.write(new_trans.encode('utf-8'))
     trans_file.close()
 
 def merge_trans(empty_trans, exists_trans):
@@ -50,18 +55,20 @@ def merge_trans(empty_trans, exists_trans):
         if not (key in empty_trans):
             keys_not_supported.append(key)
     for key in keys_not_supported:
-        del exists_trans[key]
-        print 'del', key
+        print 'Cannot find Key', key, 'in template, delete it? (y/n):' ,
+        if raw_input().strip() == 'y':
+            del exists_trans[key]
     empty_trans.update(exists_trans)
     return empty_trans
 
 def format_out(trans):
-    str = json.dumps(trans)
-    str = str.replace('}}', '}\n}')
-    str = str.replace('{"', '{\n    "').replace('"}', '"\n  }')
-    str = str.replace('}, ', '}\n, ').replace('", "', '",\n    "')
-    return str
-
+    arr = []
+    for k, v in trans.items():
+        sub_arr = []
+        for sub_k, sub_v in v.items():
+            sub_arr.append('\t\t"%s": "%s"' % (sub_k, sub_v))
+        arr.append(('\t"%s": {\n' % k )+ ',\n'.join(sub_arr) + '\n\t}')
+    return '{\n'+',\n'.join(arr)+'\n}'
 
 if __name__ == '__main__':
     keys = scan_template()
