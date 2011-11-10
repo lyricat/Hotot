08602837154e5fd8485af793b7f6710eb69f26fe
Shellex Wai <5h3ll3x@gmail.com>
2011-11-10 11:37:36 +0800

    fixed: undeclared variable.

--- data/js/ui.main.js
+++ data/js/ui.main.js
@@ -175,7 +175,7 @@ function load_tweet_success(self, json) {
     }
     // push state
     if (util.is_native_platform()) {
-        hotot_action('system/incoming/' + group + '/' 
+        hotot_action('system/incoming/' + self.name + '/' 
             + encodeURIComponent(JSON.stringify(tweets)))
     }
     return json.length;
