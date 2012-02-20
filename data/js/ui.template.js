e0e4f809fbc0df01fbe88ba6213bdd2444859ccd
lb1a <luke.b3103@gmail.com>
2012-02-20 09:23:31 +0100

    prettyfication of reply status
    using the same format for reply and reply all.
    editited template.

--- data/js/ui.template.js
+++ data/js/ui.template.js
@@ -856,9 +856,9 @@ function form_retweeted_by(tweet_obj, pagename) {
     var reply_id = tweet_obj.hasOwnProperty('in_reply_to_status_id_str')
             ? tweet_obj.in_reply_to_status_id_str:tweet_obj.in_reply_to_status_id;    
     var reply_str = (reply_id != null) ?
-        _('reply to') + ' <a class="who_href" href="#'
+        _('Reply to @') + ' <a class="who_href" href="#'
             + reply_name + '">'
-            + reply_name + '</a>'
+            + reply_name + '</a>: '
         : '';
 
     var timestamp = Date.parse(tweet_obj.created_at);
