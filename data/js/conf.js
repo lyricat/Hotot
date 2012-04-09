edddfdad417da298abe80e9aadd9b4c906fd693c
Jiahua Huang <jhuangjiahua@gmail.com>
2012-04-10 06:50:44 +0800

    Chrome app bump to 0.9.7.56

--- data/js/conf.js
+++ data/js/conf.js
@@ -3,7 +3,7 @@ conf = {
 
 vars: {
       'platform': 'Linux'
-    , 'version': '0.9.7.55'
+    , 'version': '0.9.7.56'
     , 'codename': 'Ada'
     , 'consumer_key': 'SCEdx4ZEOO68QDCTC7FFUQ'
     , 'consumer_secret': '2IBoGkVrpwOo7UZhjkYYekw0ciXG1WHpsqQtUqZCSw'
