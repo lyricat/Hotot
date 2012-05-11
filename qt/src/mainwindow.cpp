c126d367eb6bea0af17dc20a85541718da365ed4
Weng Xuetian <wengxt@gmail.com>
2012-05-11 20:37:43 +0800

    seems stream not work is qt problem, I could only workaround it under KDE.

--- qt/src/mainwindow.cpp
+++ qt/src/mainwindow.cpp
@@ -44,6 +44,7 @@
 
 #ifdef HAVE_KDE
 #include <KWindowSystem>
+#include <KIO/AccessManager>
 #endif
 
 #ifdef MEEGO_EDITION_HARMATTAN
@@ -129,6 +130,9 @@ MainWindow::MainWindow(bool socks, QWidget *parent) :
 #endif
 
     m_page = new HototWebPage(this);
+#ifdef HAVE_KDE
+    m_page->setNetworkAccessManager(new KIO::Integration::AccessManager(m_page));
+#endif
 
 #ifdef Q_OS_UNIX
     QDir dir(QDir::homePath().append("/.config/hotot-qt"));
