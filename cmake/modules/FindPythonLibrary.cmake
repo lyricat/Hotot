2473cbe4c4f847ca87ddbd1c417a888d2ece81d6
Yichao Yu <yyc1992@gmail.com>
2012-05-24 16:23:51 -0400

    add support for python3 use pkgconfig if availible, relicense findpythonlibrary

--- cmake/modules/FindPythonLibrary.cmake
+++ cmake/modules/FindPythonLibrary.cmake
@@ -4,30 +4,57 @@
 #
 # This file defines the following variables:
 #
-# PYTHON_EXECUTABLE - The path and filename of the Python interpreter.
+# PYTHON_EXECUTABLE
+#     The path and filename of the Python interpreter.
 #
-# PYTHON_SHORT_VERSION - The version of the Python interpreter found,
+# PYTHON_SHORT_VERSION
+#     The version of the Python interpreter found,
 #     excluding the patch version number. (e.g. 2.5 and not 2.5.1))
 #
-# PYTHON_LONG_VERSION - The version of the Python interpreter found as a human
-#     readable string.
+# PYTHON_LONG_VERSION
+#     The version of the Python interpreter found as a human readable string.
 #
-# PYTHON_SITE_PACKAGES_INSTALL_DIR - this cache variable can be used for installing 
-#                              own python modules. You may want to adjust this to be the
-#                              same as ${PYTHON_SITE_PACKAGES_DIR}, but then admin
-#                              privileges may be required for installation.
+# PYTHON_SITE_PACKAGES_INSTALL_DIR
+#     This cache variable can be used for installing own python modules.
+#     You may want to adjust this to be the same as
+#     ${PYTHON_SITE_PACKAGES_DIR}, but then admin privileges may be required
+#     for installation.
 #
-# PYTHON_SITE_PACKAGES_DIR - Location of the Python site-packages directory.
+# PYTHON_SITE_PACKAGES_DIR
+#     Location of the Python site-packages directory.
 #
-# PYTHON_INCLUDE_PATH - Directory holding the python.h include file.
+# PYTHON_INCLUDE_PATH
+#     Directory holding the python.h include file.
 #
-# PYTHON_LIBRARY, PYTHON_LIBRARIES- Location of the Python library.
+# PYTHON_LIBRARY, PYTHON_LIBRARIES
+#     Location of the Python library.
+#
+# PYTHON_MAGIC_TAG
+#     The magic tag used in byte compiling (PEP 3147)
 
-# Copyright (c) 2007, Simon Edwards <simon@simonzone.com>
-# Redistribution and use is allowed according to the terms of the BSD license.
-# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+#   Copyright (C) 2012~2012 by Yichao Yu
+#   yyc1992@gmail.com
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, version 2 of the License.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the
+#   Free Software Foundation, Inc.,
+#   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-
+# This file incorporates work covered by the following copyright and
+# permission notice:
+#
+#     Copyright (c) 2007, Simon Edwards <simon@simonzone.com>
+#     Redistribution and use is allowed according to the terms of the BSD
+#     license. For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
 include(CMakeFindFrameworks)
 
@@ -35,25 +62,23 @@ if(EXISTS PYTHON_LIBRARY)
   # Already in cache, be silent
   set(PYTHONLIBRARY_FOUND TRUE)
 else(EXISTS PYTHON_LIBRARY)
-
   find_package(PythonInterp)
 
   if(PYTHONINTERP_FOUND)
-
     # get the directory of the current file, used later on in the file
-    get_filename_component( _py_cmake_module_dir  ${CMAKE_CURRENT_LIST_FILE} PATH)
+    get_filename_component(_py_cmake_module_dir ${CMAKE_CURRENT_LIST_FILE} PATH)
     if(NOT EXISTS "${_py_cmake_module_dir}/FindLibPython.py")
       message(FATAL_ERROR "The file FindLibPython.py does not exist in ${_py_cmake_module_dir} (the directory where FindPythonLibrary.cmake is located). Check your installation.")
     endif(NOT EXISTS "${_py_cmake_module_dir}/FindLibPython.py")
 
-    execute_process(COMMAND ${PYTHON_EXECUTABLE}  "${_py_cmake_module_dir}/FindLibPython.py" OUTPUT_VARIABLE python_config)
+    execute_process(COMMAND ${PYTHON_EXECUTABLE} "${_py_cmake_module_dir}/FindLibPython.py" OUTPUT_VARIABLE python_config)
     if(python_config)
       string(REGEX REPLACE ".*exec_prefix:([^\n]+).*$" "\\1" PYTHON_PREFIX ${python_config})
       string(REGEX REPLACE ".*\nshort_version:([^\n]+).*$" "\\1" PYTHON_SHORT_VERSION ${python_config})
       string(REGEX REPLACE ".*\nlong_version:([^\n]+).*$" "\\1" PYTHON_LONG_VERSION ${python_config})
-
       string(REGEX REPLACE ".*\npy_inc_dir:([^\n]+).*$" "\\1" _TMP_PYTHON_INCLUDE_PATH ${python_config})
       string(REGEX REPLACE ".*\nsite_packages_dir:([^\n]+).*$" "\\1" _TMP_PYTHON_SITE_PACKAGES_DIR ${python_config})
+      string(REGEX REPLACE ".*\nmagic_tag:([^\n]*).*$" "\\1" PYTHON_MAGIC_TAG ${python_config})
 
       # Put these two variables in the cache so they are visible for the user, but read-only:
       set(PYTHON_INCLUDE_PATH "${_TMP_PYTHON_INCLUDE_PATH}" CACHE PATH "The python include directory" FORCE)
@@ -63,9 +88,17 @@ else(EXISTS PYTHON_LIBRARY)
       set(PYTHON_SITE_PACKAGES_INSTALL_DIR ${_TMP_PYTHON_SITE_PACKAGES_DIR} CACHE PATH "The directory where python modules will be installed to.")
 
       string(REGEX REPLACE "([0-9]+).([0-9]+)" "\\1\\2" PYTHON_SHORT_VERSION_NO_DOT ${PYTHON_SHORT_VERSION})
-      set(PYTHON_LIBRARY_NAMES python${PYTHON_SHORT_VERSION} python${PYTHON_SHORT_VERSION_NO_DOT})
+      find_package(PkgConfig)
+      if(PKG_CONFIG_FOUND)
+        pkg_check_modules(PYTHON_PKG python-${PYTHON_SHORT_VERSION})
+        set(PYTHON_LIBRARY_NAMES ${PYTHON_PKG_LIBRARIES})
+      endif(PKG_CONFIG_FOUND)
+      # add fallback
+      set(PYTHON_LIBRARY_NAMES ${PYTHON_LIBRARY_NAMES}
+        python${PYTHON_SHORT_VERSION}
+        python${PYTHON_SHORT_VERSION_NO_DOT})
       if(WIN32)
-          string(REPLACE "\\" "/" PYTHON_SITE_PACKAGES_DIR ${PYTHON_SITE_PACKAGES_DIR})
+        string(REPLACE "\\" "/" PYTHON_SITE_PACKAGES_DIR ${PYTHON_SITE_PACKAGES_DIR})
       endif(WIN32)
       find_library(PYTHON_LIBRARY NAMES ${PYTHON_LIBRARY_NAMES} PATHS ${PYTHON_PREFIX}/lib ${PYTHON_PREFIX}/libs NO_DEFAULT_PATH)
       set(PYTHONLIBRARY_FOUND TRUE)
@@ -102,5 +135,4 @@ else(EXISTS PYTHON_LIBRARY)
       message(FATAL_ERROR "Could not find Python")
     endif(PYTHONLIBRARY_FIND_REQUIRED)
   endif(PYTHONLIBRARY_FOUND)
-
-endif (EXISTS PYTHON_LIBRARY)
+endif(EXISTS PYTHON_LIBRARY)
