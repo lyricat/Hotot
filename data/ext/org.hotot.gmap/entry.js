02fba1fdde679c3ed17fdc9bee1eca6543dd576b
Aveline Swan <i@vii.im>
2013-01-26 18:21:53 +0800

    Use Google Maps Image API Instead

--- data/ext/org.hotot.gmap/entry.js
+++ data/ext/org.hotot.gmap/entry.js
@@ -24,7 +24,15 @@ function on_form_indicator(tweet, html) {
     if (tweet.geo && tweet.geo.type == 'Point') {
         var x = tweet.geo.coordinates[0];
         var y = tweet.geo.coordinates[1];
-        var indicator = '<a class="geo_indicator" href="javascript:void(0);" x="'+x+'" y="'+y+'" onclick="ext.HototGMap.on_map_indicator_clicked('+ x + ',' + y + ')" style="background: transparent url(ext/'+ext.HototGMap.id+'/ic16_marker.png) no-repeat; width: 16px; height: 16px; display:inline-block;"></a>';
+        var tag = "geo1_" + tweet.id + Date.now();
+        console.log(tag);
+        setTimeout(function() {
+            $('#' + tag).click(function(e) {
+                e.preventDefault();
+                return ext.HototGMap.on_map_indicator_clicked(x, y);
+            })
+        }, 500);
+        var indicator = '<a class="geo_indicator" href="#" x="'+x+'" y="'+y+'" id="' + tag + '" style="background: transparent url(ext/'+ext.HototGMap.id+'/ic16_marker.png) no-repeat; width: 16px; height: 16px; display:inline-block;"></a>';
         html += indicator;
     }
     return html;
@@ -33,12 +41,14 @@ function on_form_indicator(tweet, html) {
 on_map_indicator_clicked:
 function on_map_indicator_clicked(x, y) {
     ext.HototGMap.map_dialog.open(); 
-    $('#hotot_gmap_frame').get(0).contentWindow.load_map(x, y);
+    $('#hotot_gmap_frame').get(0).contentWindow.document.getElementById('map').innerHTML = '<img src="http://maps.googleapis.com/maps/api/staticmap?center=' + x + ',' + y + '&zoom=11&size=' + ($('#ext_hotot_gmap_map_dialog').width() - 2) + 'x' + ($('#ext_hotot_gmap_map_dialog').height() - 40) + '&sensor=false">';
+
     $('#hotot_gmap_frame').css({
           'height': ($('#ext_hotot_gmap_map_dialog').height() - 30) + 'px'
         , 'width': ($('#ext_hotot_gmap_map_dialog').width() - 2)+'px'
         , 'padding': '0'});
 },
+
 
 create_map_dialog:
 function create_map_dialog() {
@@ -53,25 +63,7 @@ function create_map_dialog() {
     ext.HototGMap.map_dialog.resize(500, 500);
     ext.HototGMap.map_doc = $('#hotot_gmap_frame').get(0).contentWindow.document;
     ext.HototGMap.map_doc.open();
-    $(ext.HototGMap.map_doc).
-    ext.HototGMap.map_doc.write("<html><head></head><body><div id=\"map\" style=\"width:100%;height:100%;\"></div></body></html>");
-    $(ext.HototGMap.map_doc).ready(function() {
-        var child = document.createElement('script');
-        child.setAttribute("type", "text/javascript");
-        child.setAttribute("src", "https://maps.google.com/maps/api/js?sensor=false");
-        child.onload = function() {
-            var myLatlng = new google.maps.LatLng(x, y);
-            var myOptions = {
-              zoom: 13,
-              center: myLatlng,
-              mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
-            var map = new google.maps.Map(document.getElementById('map'), myOptions);
-            var marker = new google.maps.Marker({position: myLatlng, title:'Here!'});
-            marker.setMap(map);
-        };
-        this.getElementsByTagName("head")[0].appendChild(child);
-    });
+    ext.HototGMap.map_doc.write("<html><head><style>*{margin: 0 0 0 0;padding:0 0 0 0;overflow:hidden;}</style></head><body><div id=\"map\" style=\"width:100%;height:100%;\"></div></body></html>");
     ext.HototGMap.map_doc.close();
 },
 
